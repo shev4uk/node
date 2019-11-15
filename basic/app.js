a139c1e69bc082124f5442496bf34bad22cdcc60
yshevc <yshevc@softserveinc.com>
2019-11-15 12:06:53 +0200

    basic

--- basic/app.js
+++ basic/app.js
@@ -1,36 +1,6 @@
 const http = require('http');
-const fs = require('fs');
+const routes = require('./routes');
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  if (url === '/') {
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html><head><title>Document</title></head><body>Form');
-    res.write('<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form>');
-    res.write('</body></html>');
-    return res.end();
-  }
-  if (url === '/message' && method === 'POST') {
-    const body = [];
-    req.on('data', (chunk) => {
-      body.push(chunk);
-    });
-    return req.on('end', () => {
-      const parseBody = Buffer.concat(body).toString();
-      const msq = parseBody.split('=')[1];
-      fs.writeFile('message.txt', msq, (err) => {
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-      });
-    });
-  }
-
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<html><head><title>Document</title></head><body>Work');
-  res.write('</body></html>');
-  return res.end();
-});
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
