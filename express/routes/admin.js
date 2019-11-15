c903ce9afbdf024f0e560c4e7553af09bb01db05
yshevc <yshevc@softserveinc.com>
2019-11-15 19:14:58 +0200

    express

---
+++ express/routes/admin.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/add-product', (req, res, next ) => {
+  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+});
+
+router.post('/product', (req, res, next ) => {
+  console.log(req.body.title);
+  res.redirect('/');
+});
+
+module.exports = router;
